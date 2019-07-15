a3145cec122843560714b6f2901bf702cc37d121
Lisonallie <allisonvanlinden@gmail.com>
2019-07-15 09:49:11 +0200

    add variable names

---
+++ app.js
@@ -0,0 +1,4 @@
+let nameAndNumber = document.getElementById("title");
+let pokemon = document.getElementById("pokeImg");
+let type = document.getElementById("pokeType");
+let stat = document.getElementById("pokeStat");
