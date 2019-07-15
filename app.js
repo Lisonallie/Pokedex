ab893f95bf682ab9d963f9d43850b39be1ecde19
Lisonallie <allisonvanlinden@gmail.com>
2019-07-15 09:54:32 +0200

    add extra div

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let nameAndNumber = document.getElementById("title");
 let pokemon = document.getElementById("pokeImg");
+let evolution = document.getElementById("evolution");
 let type = document.getElementById("pokeType");
 let stat = document.getElementById("pokeStat");
