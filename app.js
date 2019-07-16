e336a82b5de67778745896ae44e8746a14eb4e0e
nadiralaoui1 <nadiralaoui23@gmail.com>
2019-07-16 10:01:21 +0200

    Solve merge conflict

--- app.js
+++ app.js
@@ -3,58 +3,52 @@ let pokemon = document.getElementById("pokeImg");
 let evolution = document.getElementById("evolution");
 let type = document.getElementById("pokeType");
 let stat = document.getElementById("pokeStat");
-let pokeNumber = document.getElementById("number");
 let pokeName = document.getElementById("name");
 
 
-title.addEventListener("input", function (){
+title.addEventListener("input", function () {
     title.value = title.value.toLowerCase();
-    
+
 });
 
 title.addEventListener("keypress", function (e) {
     if (e.keyCode == 13 || e.which == 13) {
         getPokemon();
+        type.textContent = "";
+        stat.textContent = "";
     }
 });
 
 async function getPokemon() {
-    let name = title.value;
+    let name = title.value
     let api = `https://pokeapi.co/api/v2/pokemon/${name}`;
     let response = await axios.get(api);
     let number = response.data.id;
+    let call = response.data.name;
     let img = response.data.sprites.front_default;
-    let typeOfPokemon = response.data.types[0].type.name;
-    let secondType = response.data.types[1].type.name;
-    
-    img.src =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`
-    pokeName.innerHTML = `#${number} ⠀⠀<strong>${name}</strong>`; 
-    pokemon.src = img;
-    
-    
-    for(i=0; i<response.data.types.length; i++) {
-        // type.textContent += `${response.data.types[i].type.name} ⠀⠀ ⠀ `;
+    console.log(response.data);
 
-        if (i = 0) {
-            type.innerHTML = `${typeOfPokemon}`;
+    if (number > 802) {
+        alert("The Pokémon fled 🚫.")
+    }
+
+    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`
+    pokeName.innerHTML = `#${number} ⠀⠀<strong>${call}</strong>`;
+
+    pokemon.src = img;
+
+    for (i = 0; i < response.data.types.length; i++) {
+        type.innerHTML += `${response.data.types[i].type.name} ⠀`;
+
+    }
+
+    for (i = 0; i < response.data.moves.length; i++) {
+        stat.innerHTML += `${response.data.moves[i].move.name}<br>`
+        if (i > 3) {
+            return;
         }
-        else {
-            type.innerHTML = `${typeOfPokemon}, ${secondType}`;
-        }
-    
-        console.log(i);
-        
     }
 
 
-    // type.innerHTML = `${typeOfPokemon}, ${secondType}`;
 
-
-    console.log(response.data);
-    console.log(response.data.types[0].type.name);
-    
-    
-    
-    
-    
 }
